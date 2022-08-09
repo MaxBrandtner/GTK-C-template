3f6df527b2dd645825253354aa16b1eb7b14b8e5
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2022-08-09 21:44:41 +0700

    replace ' with " in generated script
    Failed attempt to fix #2, but still a good change to make, as files / directories with ' can exist.

--- generate_gresource_xml.sh
+++ generate_gresource_xml.sh
@@ -5,10 +5,10 @@ n_ui_files=$(wc -l < $2)
 
 rm $1.gresource.xml
 
-echo "<?xml version='1.0' encoding='UTF-8'?>" 			>> $1.gresource.xml
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" 		>> $1.gresource.xml
 echo "<!-- Generated with generate-gresource-xml.sh  -->"	>> $1.gresource.xml
 echo "<gresources>" 						>> $1.gresource.xml
-echo "	<gresource prefix='"$local_directory"'>"		>> $1.gresource.xml
+echo "	<gresource prefix=\""$local_directory"\">"		>> $1.gresource.xml
 
 while IFS= read -r line
 do
